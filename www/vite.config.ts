48d664586ca9997cb71c579c87738ffdf8e80b18
Rei-Ashine <52484735+Rei-Ashine@users.noreply.github.com>
2026-07-09 00:33:41 +0900

    Upgrade Vite to v8 and clean up configuration

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     wasm(),
-    topLevelAwait(),
     react()
   ],
   worker: {
     format: 'es',
-    plugins: [
+    plugins: () => [
       wasm()
     ]
+  },
+  build: {
+    target: 'esnext'
   },
   base: '/wasm-fractal/',
 });
